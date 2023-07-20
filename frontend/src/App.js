5d55c2beb7c7556e4f128b00809789fd798c5578
Rowan Al Qassab <Rowanalqassab@Rowans-MacBook-Pro.local>
2023-07-21 01:26:27 +0300

    Navbar and homepage

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,24 @@
 import React from 'react'
-import ProductForm from './components/product/ProductForm';
+import LogoImg from '../src/images/Diamond.png'
+import HomePage from './components/home/HomePage'
 
 export default function App() {
   return (
     <div>
-      <h1>The Vintage Auction</h1>
-      <ProductForm />
+      <div className="logo-nav">
+        <img className="logo-img" src={LogoImg} />
+        <h1 className="logo-title">The Vintage Auction</h1>
+        <div className="nav">
+          <ul id="nav-bar">
+            <li className="nav-li">Home</li>
+            <li className="nav-li">Shop</li>
+            <li className="nav-li">Profile</li>
+            <li className="nav-li">Login</li>
+            <li className="nav-li">Logout</li>
+          </ul>
+        </div>
+      </div>
+      <HomePage/>
     </div>
   )
 }
---
+++ frontend/src/components/home/HomePage.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export default function HomePage() {
+  return (
+    <div>
+        <div className="main-homepage">
+            <div className="homepage-div1"></div>
+            <div className="homepage-div2"></div>
+        </div>
+    </div>
+  )
+}
